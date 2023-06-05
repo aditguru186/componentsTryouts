9d2940c3521bf34274e9be0d9951d83d4cbdc567
Aditya <aditguru186@gmail.com>
2023-06-06 01:40:20 +0530

    Circle Color complete

--- change-color-2-circles/src/App.js
+++ change-color-2-circles/src/App.js
@@ -1,58 +1,13 @@
 import React, { useEffect, useState } from "react";
-
-let Circle = ({id,radius,color }) =>{
-  console.log("CirlceId : "+id+" Radius : "+radius)
-  return(
-    <div
-          id={id}
-          style={{
-            width: radius,
-            height:radius,
-            borderRadius: "50%",
-            backgroundColor: color
-          }}
-        />
-  )
-}
+import TwoColoredCircle from './TwoColoredCircle';
+// import {Tabs, Tab} from "@material-ui/core"
 
 var App = () => {
-  const [circle1Radius, setCircle1Radius] = useState(100);
-  const [circle2Radius, setCircle2Radius] = useState(100);
-  const [circle1Color, setCircle1Color] = useState("#aafffb");
-  const [circle2Color, setCircle2Color] = useState("#aafffb");
-  const [circleSelected, setCircleSelected] = useState(null);
-
-  useEffect(() => {
-    const handleClick = (event) => {
-      const circleId = event.target.id;
-      setCircleSelected(circleId);
-    };
-    document.getElementById("circle1").addEventListener("click", handleClick);
-    document.getElementById("circle2").addEventListener("click", handleClick);
-  }, []);
-  return (
+  return(
     <div>
-      <div style={{padding:"20px", display: "flex", "align-items": "center"}}>
-        <Circle id={'circle1'} radius={circle1Radius} color={circle1Color}/>
-        <Circle id={'circle2'} radius={circle2Radius} color={circle2Color}/>
-      </div>
-      <p>Selected Circle is : {circleSelected}</p>
-      <p>Radius : {circleSelected === 'circle1' ? circle1Radius : circle2Radius}</p>
-      <p>Color : {circleSelected === 'circle1' ? circle1Color : circle2Color}</p>
-      <input
-        type="color"
-        onChange={(event) => { (circleSelected == "circle1") ?
-            setCircle1Color(event.target.value) : setCircle2Color(event.target.value);
-        }}
-      />
-      <input type="text"
-      placeholder="Radius To be Entered here"
-      onChange={(event)=>{
-        (circleSelected === "circle1") ? setCircle1Radius(event.target.value): setCircle2Radius(event.target.value);
-      }}/>
-
+      <TwoColoredCircle/>
     </div>
-  );
+  )
 };
 
 export default App;
